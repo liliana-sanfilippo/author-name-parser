f599dc37c0d8d268c4dd136a1ed5481985f01583
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2025-08-14 11:10:36 +0200

    Added tests for name lists

--- test/name_lists.test.ts
+++ test/name_lists.test.ts
@@ -20,9 +20,17 @@ describe('test for parsing lists of names', function () {
     });
     describe('Test 2. - only names with mixed seperators', function (){
         it('should return a list of correctly seperated names', function () {
-            const input = "";
+            const input = "Hoffmann, Stefan A. and Diggans, James, Densmore, Douglas; Dai, Junbiao; Knight, Tom and Leproust, Emily , Boeke, Jef D. ,and Wheeler, Nicole, and Cai, Yizhi";
             const res: FullName[] = [
-
+                {firstnames: "Stefan A.", lastname: "Hoffmann"},
+                {firstnames: "James", lastname: "Diggans"},
+                {firstnames: "Douglas", lastname: "Densmore"},
+                {firstnames: "Junbiao", lastname: "Dai"},
+                {firstnames: "Tom", lastname: "Knight"},
+                {firstnames: "Emily", lastname: "Leproust"},
+                {firstnames: "Jef D.", lastname: "Boeke"},
+                {firstnames: "Nicole", lastname: "Wheeler"},
+                {firstnames: "Yizhi", lastname: "Cai"}
             ]
             console.log(extractAllNames(input))
             expect(extractAllNames(input)).to.eql(res);
@@ -31,9 +39,11 @@ describe('test for parsing lists of names', function () {
     });
     describe('Test 3. - names and et al', function (){
         it('should return a list of correctly seperated names', function () {
-            const input = "";
+            const input = "Yousef, H., Alhajj, M., Fakoya, AO, et al.";
             const res: FullName[] = [
-
+                {firstnames: "H.", lastname: "Yousef"},
+                {firstnames: "M.", lastname: "Alhajj"},
+                {firstnames: "AO", lastname: "Fakoya"}
             ]
             console.log(extractAllNames(input))
             expect(extractAllNames(input)).to.eql(res);
@@ -42,9 +52,10 @@ describe('test for parsing lists of names', function () {
     });
     describe('Test 4. - only names with comma seperators', function (){
         it('should return a list of correctly seperated names', function () {
-            const input = "";
+            const input = "Bielecka, Anna, Mohammadi, Ali Akbar";
             const res: FullName[] = [
-
+                {firstnames: "Anna", lastname: "Bielecka"},
+                {firstnames: "Ali Akbar", lastname: "Mohammadi"}
             ]
             console.log(extractAllNames(input))
             expect(extractAllNames(input)).to.eql(res);
@@ -53,9 +64,34 @@ describe('test for parsing lists of names', function () {
     });
     describe('Test 5. - only names with and', function (){
         it('should return a list of correctly seperated names', function () {
-            const input = "";
+            const input = "Reisman, John J. and Rivington-Law, Betty and Corey, Mary and Marcotte, Jacques and Wannamaker, Eleanor and Harcourt, Dawn and Levison, Henry";
             const res: FullName[] = [
+                {firstnames: "John J.", lastname: "Reisman"},
+                {firstnames: "Betty", lastname: "Rivington-Law"},
+                {firstnames: "Mary", lastname: "Corey"},
+                {firstnames: "Jacques", lastname: "Marcotte"},
+                {firstnames: "Eleanor", lastname: "Wannamaker"},
+                {firstnames: "Dawn", lastname: "Harcourt"},
+                {firstnames: "Henry", lastname: "Levison"}
+            ]
+            console.log(extractAllNames(input))
+            expect(extractAllNames(input)).to.eql(res);
 
+        });
+    });
+    describe('Test 6. - only names with and seperators', function (){
+        it('should return a list of correctly seperated names', function () {
+            const input = "Hoffmann, Stefan A. and Diggans, James and Densmore, Douglas and Dai, Junbiao and Knight, Tom and Leproust, Emily and Boeke, Jef D. and Wheeler, Nicole and Cai, Yizhi";
+            const res: FullName[] = [
+                {firstnames: "Stefan A.", lastname: "Hoffmann"},
+                {firstnames: "James", lastname: "Diggans"},
+                {firstnames: "Douglas", lastname: "Densmore"},
+                {firstnames: "Junbiao", lastname: "Dai"},
+                {firstnames: "Tom", lastname: "Knight"},
+                {firstnames: "Emily", lastname: "Leproust"},
+                {firstnames: "Jef D.", lastname: "Boeke"},
+                {firstnames: "Nicole", lastname: "Wheeler"},
+                {firstnames: "Yizhi", lastname: "Cai"}
             ]
             console.log(extractAllNames(input))
             expect(extractAllNames(input)).to.eql(res);
